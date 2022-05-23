161eb31b2bfc39f3c6efaccd71d04c8d7ec991db
ChimaG <chimagee@mymusictaste.com>
2022-05-23 13:00:02 +0800

    fix: add text Class

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,8 +5,9 @@ interface ToggleButtonProps {
     selectedIdx: number;
     containerClass?: string;
     toggleClass?: string;
+    textClass?: string;
 }
 
-declare const ToggleButton: ({ onSelect, items, toggleClass, containerClass, selectedIdx: selectedIdxProps, }: ToggleButtonProps) => JSX.Element;
+declare const ToggleButton: ({ onSelect, items, toggleClass, containerClass, textClass, selectedIdx: selectedIdxProps, }: ToggleButtonProps) => JSX.Element;
 
 export { ToggleButton };
