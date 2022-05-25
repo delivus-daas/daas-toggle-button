ab6a16c268a0e415011033a7810461201365131b
Manoj <manojjonam10@gmail.com>
2022-05-25 17:37:47 +0700

    initial commit

--- src/stories/ToggleButton.stories.tsx
+++ src/stories/ToggleButton.stories.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Story } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import ToggleButton from "../components/toggleButton";
 import { ToggleButtonProps } from "../components/toggleButton/ToggleButton.types";
 
 export default {
   title: "ToggleButton",
   component: ToggleButton,
-};
+} as ComponentMeta<typeof ToggleButton>;
 
 const Template: Story<ToggleButtonProps> = (args) => <ToggleButton {...args} />;
 
@@ -20,4 +23,34 @@ export const Secondary = Template.bind({});
 Secondary.args = {
   selectedIdx: 1,
   items: ["업 신청", "상차 대기", "반납 업무", "완한 업무"],
+};
+Primary.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root element
+  const canvas = within(canvasElement);
+  await expect(canvas.getByText('RIDER'))
+  await expect(canvas.getByText('SELLER'))
+  await expect(canvas.getByText('STAFF'))
+
+  await userEvent.hover(canvas.getByText('RIDER'))
+  await userEvent.click(canvas.getByText('RIDER'))
+
+  await userEvent.hover(canvas.getByText('SELLER'))
+  await userEvent.click(canvas.getByText('SELLER'))
+
+  await userEvent.hover(canvas.getByText('STAFF'))
+  await userEvent.click(canvas.getByText('STAFF'))
+};
+
+Secondary.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root element
+  const canvas = within(canvasElement);
+  await expect(canvas.getByText('업 신청'))
+  await expect(canvas.getByText('상차 대기'))
+  await expect(canvas.getByText('반납 업무'))
+  await expect(canvas.getByText('완한 업무'))
+
+  await userEvent.click(canvas.getByText('업 신청'))
+  await userEvent.click(canvas.getByText('상차 대기'))
+  await userEvent.click(canvas.getByText('반납 업무'))
+  await userEvent.click(canvas.getByText('완한 업무'))
 };
